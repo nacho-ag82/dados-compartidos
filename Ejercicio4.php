85c84d20e4420ad506919e3ad7b75f53778056f5
nacho-ag82 <iagugar398@g.educaand.es>
2024-10-16 12:01:22 +0200

    finalizado

--- Ejercicio4.php
+++ Ejercicio4.php
@@ -13,35 +13,19 @@ echo "<h3>Dado a eliminar</h3>";
 $numeroAleatorio = rand(1,6);
 $dadoEliminar = $arrayDados[$numeroAleatorio];
 mostrarImagen($dadoEliminar);
+echo "<h3>Dados Restantes<h3><br>";
 
+$arrayDados= array_diff($arrayDados,   array($dadoEliminar));
+
+foreach ($arrayDados as $dados) { 
+    mostrarImagen($dados);
+}
 
 //Creo una funcion con un switch que dependiendo del numero del dado muestre una imagen u otra
 //La funcion la he hecho porque es un codigo que voy a volver a reutilzar
 function mostrarImagen($dado){
-    switch($dado){
-        case 1:
-            echo"<img src='Imagenes/1.svg' alt='Dado 1'/>";
-            break;
-
-        case 2:
-            echo"<img src='Imagenes/2.svg' alt='Dado 2'/>";
-            break;
-            
-        case 3:
-            echo"<img src='Imagenes/3.svg' alt='Dado 3'/>";
-            break;
-                    
-        case 4:
-            echo"<img src='Imagenes/4.svg' alt='Dado 4'/>";
-            break;
-
-        case 5:
-            echo"<img src='Imagenes/5.svg' alt='Dado 5'/>";
-            break;
-
-        case 6:
-            echo"<img src='Imagenes/6.svg' alt='Dado 6'/>";
-            break;
-    }
+    echo"<img src='$dado.svg' alt='Dado 1'/>";   
 }
+
+
 ?>
