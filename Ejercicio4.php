f2b884cc1fec06fd7df53f4215ba7f999dd446ed
nacho-ag82 <iagugar398@g.educaand.es>
2024-10-16 12:21:10 +0200

    nachoyafishforfin

--- Ejercicio4.php
+++ Ejercicio4.php
@@ -1,6 +1,8 @@
 <?php 
+//Creo un array para meter los dados generados aleatoriamente
 $arrayDados =[];
 
+//Muestro la tirada de los 6 dados, cada dado cun un numero aleatorio generado, ademas muestro cada uno de los dados
 echo "<h3>Tirada de 6 dados</h3>";
 for ($i = 0; $i < 6; $i++){
     $dadoAleatorio = rand(1,6);
@@ -8,15 +10,16 @@ for ($i = 0; $i < 6; $i++){
     mostrarImagen($dadoAleatorio);
 }
 
-
+//Muestro el dado que quiero eliminar
 echo "<h3>Dado a eliminar</h3>";
-$numeroAleatorio = rand(1,6);
-$dadoEliminar = $arrayDados[$numeroAleatorio];
+$dadoEliminar = rand(1,6);
 mostrarImagen($dadoEliminar);
-echo "<h3>Dados Restantes<h3><br>";
 
+//Elimino el dado a eliminar del array de dados
+echo "<h3>Dados Restantes<h3><br>";
 $arrayDados= array_diff($arrayDados,   array($dadoEliminar));
 
+//Muestro el array de dados sin el dado a eliminar, ya que lo he eliminado previamente
 foreach ($arrayDados as $dados) { 
     mostrarImagen($dados);
 }
@@ -24,6 +27,6 @@ foreach ($arrayDados as $dados) {
 //Creo una funcion con un switch que dependiendo del numero del dado muestre una imagen u otra
 //La funcion la he hecho porque es un codigo que voy a volver a reutilzar
 function mostrarImagen($dado){
-    echo"<img src='$dado.svg' alt='Dado 1'/>";   
+    echo"<img src='Imagenes/$dado.svg' alt='Dado 1'/>";   
 }
 ?>
